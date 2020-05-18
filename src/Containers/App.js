cab8ab1df173a750d3f4bb1f8ff07f45a0c56188
Pranav Sood <pranav@Pranavs-MacBook-Pro.local>
2020-05-18 11:51:54 -0400

    Robofriends from laptop

---
+++ src/Containers/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import CardList from '../Components/CardList';
+import { robots } from '../robots';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/scroll';
+import ErrorBoundary from './ErrorBoundary.js';
+
+
+class App extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			robots:[],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users => this.setState({robots: robots}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value })
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		return (
+			<div className ='tc'>
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange = {this.onSearchChange}/>
+				<Scroll>
+				<ErrorBoundary>
+					<CardList robots={filteredRobots}/>
+				</ErrorBoundary>
+				</Scroll>
+			</div>
+		)
+	}
+}
+
+export default App;
